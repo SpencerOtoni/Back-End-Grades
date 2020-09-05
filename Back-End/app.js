719861fe8c34de574fcf969899952b9eeb387a4c
Spencer <sspencerotoni@gmail.com>
2020-09-05 19:57:56 -0300

    origin

--- Back-End/app.js
+++ Back-End/app.js
@@ -24,9 +24,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
-  cors({
+  cors(/*{
     origin: 'https://front-end-grades.herokuapp.com',
-  })
+  }*/)
 );
 app.use(gradeRouter)
 
