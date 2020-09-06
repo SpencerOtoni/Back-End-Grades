9962fb8d3f83f4313b4d4746d9fae0161ba868e5
Spencer <sspencerotoni@gmail.com>
2020-09-06 19:54:01 -0300

    controller

--- Back-End/controllers/gradeController.js
+++ Back-End/controllers/gradeController.js
@@ -99,7 +99,6 @@ const remove = async (req, res) => {
 
   try {
     const gradeDelete = await gradeModel.findByIdAndRemove({ _id: id });
-    res.send(gradeDelete)
     if (!gradeDelete) {
       res.status(404).send("Nao encontrado nenhum grade para excluir");
     }
